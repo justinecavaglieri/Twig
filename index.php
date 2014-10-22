274845ec0d28bee276eb2c89cb29eaaf445a3ccc
justinecavaglieri <justine.cavaglieri@hotmail.fr>
2014-10-22 13:01:00 +0200

    remove slugify

--- index.php
+++ index.php
@@ -8,4 +8,20 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+Twig_Autoloader::register();
 
+$loader = new Twig_Loader_Filesystem([
+    __DIR__.'/views',
+]);
+
+$twig = new Twig_Environment($loader, [
+    //'cache' => null,
+]);
+
+$article = [
+    'name' => 'djfghjgkj',
+    'content' => 'sdfghjk',
+];
+echo $twig->render('article.html.twig', [
+    'article' => $article,
+]);
